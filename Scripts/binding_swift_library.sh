8a04f373c1c2bb8a2f8b9c35765bbaf1f016bfa4
Iga <iga@entap.co.jp>
2022-01-17 11:49:50 +0900

    [update]Scripts/binding_swift_library.sh : xcframeworkではなくfat frameworksを生成するよう変更
    xcframeworkでは、ライブラリ化した際にPlatform(iPhone/iPhonesimulator)に応じたライブラリの読み込みができないため。
    Fatライブラリの生成において、以下のエラーとなるため
    シミュレータビルド時は「EXCLUDED_ARCHS="arm64"」を指定するよう変更
    LineSDK have the same architectures (arm64) and can't be in the same fat output file
    https://stackoverflow.com/questions/64022291/ios-14-lipo-error-while-creating-library-for-both-device-and-simulator

--- Scripts/binding_swift_library.sh
+++ Scripts/binding_swift_library.sh
@@ -22,13 +22,25 @@ function echo_warning () {
 # frameworkをビルドする
 # $1 : scheme(e.g. iphoneos, iphonesimulator)
 function build_framework () {
-	xcodebuild \
-		'ENABLE_BITCODE=NO' \
-		'BUILD_LIBRARY_FOR_DISTRIBUTION=YES' \
-		-sdk $1$IOS_SDK_VERSION  \
-		-project "$SWIFT_PROJECT_PATH"  \
-		-configuration Release  \
-		-target $OBJC_NAME
+	if [[ $1 == "iphonesimulator" ]]; then
+		xcodebuild \
+			'ENABLE_BITCODE=NO' \
+			'BUILD_LIBRARY_FOR_DISTRIBUTION=YES' \
+			'EXCLUDED_ARCHS="arm64"' \
+			-sdk $1$IOS_SDK_VERSION  \
+			-project "$SWIFT_PROJECT_PATH"  \
+			-configuration Release  \
+			-target $OBJC_NAME
+	else
+		xcodebuild \
+			'ENABLE_BITCODE=NO' \
+			'BUILD_LIBRARY_FOR_DISTRIBUTION=YES' \
+			-sdk $1$IOS_SDK_VERSION  \
+			-project "$SWIFT_PROJECT_PATH"  \
+			-configuration Release  \
+			-target $OBJC_NAME
+	fi
+
     return 0
 }
 
@@ -42,6 +54,25 @@ function create_xcframework () {
 		-framework $1 \
 		-framework $2 \
 		-output $3
+    return 0
+}
+
+function create_fat_frameworks () {
+	echo "Create fat binaries for Release-iphoneos and Release-iphonesimulator configuration"
+	echo "Copy one build as a fat framework"
+	cp -R "$SWIFT_BUILD_PATH/Release-iphoneos" "$SWIFT_BUILD_PATH/Release-fat"
+
+	echo "Combine modules from another build with the fat framework modules"
+	cp -R "$SWIFT_BUILD_PATH/Release-iphonesimulator/$SWIFT_PROJECT_NAME.framework/Modules/$SWIFT_PROJECT_NAME.swiftmodule/" "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework/Modules/$SWIFT_PROJECT_NAME.swiftmodule/"
+	cp -R "$SWIFT_BUILD_PATH/Release-iphonesimulator/$OBJC_NAME.framework/Modules/$OBJC_NAME.swiftmodule/" "$SWIFT_BUILD_PATH/Release-fat/$OBJC_NAME.framework/Modules/$OBJC_NAME.swiftmodule/"
+
+	echo "Combine iphoneos + iphonesimulator configuration as fat libraries"
+	lipo -create -output "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework/$SWIFT_PROJECT_NAME" "$SWIFT_BUILD_PATH/Release-iphoneos/$SWIFT_PROJECT_NAME.framework/$SWIFT_PROJECT_NAME" "$SWIFT_BUILD_PATH/Release-iphonesimulator/$SWIFT_PROJECT_NAME.framework/$SWIFT_PROJECT_NAME"
+	lipo -create -output "$SWIFT_BUILD_PATH/Release-fat/$OBJC_NAME.framework/$OBJC_NAME" "$SWIFT_BUILD_PATH/Release-iphoneos/$OBJC_NAME.framework/$OBJC_NAME" "$SWIFT_BUILD_PATH/Release-iphonesimulator/$OBJC_NAME.framework/$OBJC_NAME"
+
+	cp -R "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework" $OUTPUT_PATH
+	cp -R "$SWIFT_BUILD_PATH/Release-fat/$OBJC_NAME.framework" $OUTPUT_PATH
+
     return 0
 }
 
@@ -78,26 +109,28 @@ if [[ $OUTPUT_PATH == "" ]];then
 	exit 1
 fi
 
-echo "Build iOS framework for simulator and device"
-rm -Rf "$SWIFT_BUILD_PATH"
-build_framework "iphoneos"
-build_framework "iphonesimulator"
+# echo "Build iOS framework for simulator and device"
+# rm -Rf "$SWIFT_BUILD_PATH"
+# build_framework "iphoneos"
+# build_framework "iphonesimulator"
 
-echo "Create xcframework"
 rm -Rf "$OUTPUT_PATH"
 mkdir "$OUTPUT_PATH"
-create_xcframework \
-	$SWIFT_BUILD_PATH"/Release-iphoneos/"$OBJC_NAME".framework" \
-	$SWIFT_BUILD_PATH"/Release-iphonesimulator/"$OBJC_NAME".framework" \
-	$OUTPUT_PATH/$OBJC_NAME".xcframework"
+
+# echo "Create xcframework"
+# create_xcframework \
+# 	$SWIFT_BUILD_PATH"/Release-iphoneos/"$OBJC_NAME".framework" \
+# 	$SWIFT_BUILD_PATH"/Release-iphonesimulator/"$OBJC_NAME".framework" \
+# 	$OUTPUT_PATH/$OBJC_NAME".xcframework"
+create_fat_frameworks
 
 echo "Generating binding api definition and structs"
 sharpie_bind \
 	iphoneos$IOS_SDK_VERSION \
 	"$OUTPUT_PATH/XamarinApiDef" \
 	$OBJC_NAME \
-	"$OUTPUT_PATH/$OBJC_NAME.xcframework/ios-arm64_armv7/$OBJC_NAME.framework/Headers/" \
-	"$OUTPUT_PATH/$OBJC_NAME.xcframework/ios-arm64_armv7/$OBJC_NAME.framework/Headers/$OBJC_NAME-Swift.h"
+	"$OUTPUT_PATH/$OBJC_NAME.framework/Headers/" \
+	"$OUTPUT_PATH/$OBJC_NAME.framework/Headers/$OBJC_NAME-Swift.h"
 
 open $OUTPUT_PATH
 
